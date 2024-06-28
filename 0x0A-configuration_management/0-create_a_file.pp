eabc7507931d3f3682ec8658bd9ac24c222290f9
KillerA-z1 <ahmedz1a2m@gmail.com>
2024-06-28 19:59:12 +0300

    Fix puppet-lint - 00

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,6 @@
-/**
- * This Puppet manifest creates a file named 'school' in the '/tmp' directory.
- * The file has the content 'I love Puppet' and the permissions are set to '0744'.
- * The owner and group of the file are set to 'www-data'.
- */
+# This Puppet manifest creates a file named 'school' in the '/tmp' directory.
+# The file has the content 'I love Puppet' and the permissions are set to '0744'.
+# The owner and group of the file are set to 'www-data'.
 file { '/tmp/school':
     ensure  => 'file',
     content => 'I love Puppet',
