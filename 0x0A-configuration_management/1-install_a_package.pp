bbf5cc757988e02f42a68246e17461e9d84ca707
KillerA-z1 <ahmedz1a2m@gmail.com>
2024-06-28 20:14:06 +0300

    adding pip3 and werkzeug

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,29 @@
 # Puppet manifest to install the Flask package.
 # This manifest uses Puppet's package resource type to ensure that the Flask package is installed.
+
+# The package resource specifies the package name as 'python3.8', the desired version as '3.8.10',
+# and the provider as 'pip3'. This means that Puppet will use the 'pip3' package provider to
+# install the 'python3.8' package with version '3.8.10'.
+
+package { 'python3.8':
+    ensure   => '3.8.10',
+    provider => 'pip3',
+}
+
 # The package resource specifies the package name as 'flask', the desired version as '2.1.0',
 # and the provider as 'pip3'. This means that Puppet will use the 'pip3' package provider to
 # install the 'flask' package with version '2.1.0'.
+
 package { 'flask':
     ensure   => '2.1.0',
+    provider => 'pip3',
+}
+
+# The package resource specifies the package name as 'werkzeug', the desired version as '2.1.1',
+# and the provider as 'pip3'. This means that Puppet will use the 'pip3' package provider to
+# install the 'werkzeug' package with version '2.1.1'.
+
+package { 'werkzeug':
+    ensure   => '2.1.1',
     provider => 'pip3',
 }
