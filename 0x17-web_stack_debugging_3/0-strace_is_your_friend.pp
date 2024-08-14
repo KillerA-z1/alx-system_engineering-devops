4b6cfb11eae26b480f796ecf568a2b2619b574bd
KillerA-z1 <Ahmedz1a2m@gmail.com>
2024-08-14 23:21:19 +0300

    Fixes an Apache 500 error by replacing a misconfigured PHP file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# Fixes an Apache 500 error by replacing a misconfigured PHP file
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
