9c01345de1d054d41186dc1989c164c72755f9e5
KillerA-z1 <Ahmedz1a2m@gmail.com>
2024-08-22 23:13:33 +0300

    Hot Fix 00

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,12 +2,9 @@
 # holberton user and open a file without any error message.
 
 # Puppet exec resource to modify the OS security configuration
-exec {'increase-file-descriptor-limit':
-  # Command to increase the file descriptor limit for all users
-  command  => 'echo "* soft nofile 65535\n* hard nofile 65535" >> /etc/security/limits.conf',
+exec {'OS security config':
+  # Command to replace 'holberton' with 'foo' in /etc/security/limits.conf
+  command => 'sed -i "s/holberton/foo/" /etc/security/limits.conf',
   # Specify the PATH environment variable for the command execution
-  path     => '/usr/bin:/usr/sbin:/bin:/usr/local/bin',
-  # Ensure the command is only run if the file descriptor limit is not already set
-  unless   => 'grep -q "65535" /etc/security/limits.conf',
-  provider => shell,
+  path    => '/usr/bin/env/:/bin/:/usr/bin/:/usr/sbin/'
 }
